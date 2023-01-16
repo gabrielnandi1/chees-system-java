3ff1a9fe5e6517cd1f5f5b91942a8e32ec907a19
Gabriel nandi <nandibiel05@gmail.com>
2023-01-16 10:32:36 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -16,4 +16,26 @@ public class Board {
         return rows;
     }
 
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    // Metodo para retornar um peça dada pela coluna
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    // sobre carga do metodo acima
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 }
