da6d7ce3e15f90fd6f56d97d9396e9d0360433ea
Gabriel nandi <nandibiel05@gmail.com>
2023-02-08 17:02:21 -0300

    Special move - castling

--- src/chessPieces/King.java
+++ src/chessPieces/King.java
@@ -2,13 +2,17 @@ package chessPieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color white) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color white, ChessMatch chessMatch) {
         super(board, white);
+        this.chessMatch = chessMatch;
 
     }
 
@@ -20,6 +24,11 @@ public class King extends ChessPiece {
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -77,6 +86,29 @@ public class King extends ChessPiece {
         p.setValues(position.getRow() + 1, position.getColumn() + 1);
         if (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // #Movimento especial castling
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            // #Movimento especial castling torre lado menor do rei
+            Position posit1 = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(posit1)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+            // #Movimento especial castling torre lado menor do rei
+            Position posit2 = new Position(position.getRow(), position.getColumn() - 4);
+            if (testRookCastling(posit2)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
         }
         return mat;
     }
